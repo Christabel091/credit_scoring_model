4ff5283c834b1416c94fcd09c4ab7bfa38b03598
CHRISTABEL <obinwosugosiorah@gmail.com>
2024-06-14 23:20:37 -0500

    extra

---
+++ .ipynb_checkpoints/verify-checkpoint.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+print("Pandas version:", pd.__version__)
+print("NumPy version:", np.__version__)
+print("Scikit-learn version:", sklearn.__version__)
+print("TensorFlow version:", tf.__version__)
+print("Seaborn version:", sns.__version__)
